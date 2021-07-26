ba85febb10205a760a9e0327d1d5e8ddaccf8602
kmj94102 <60861383+kmj94102@users.noreply.github.com>
2021-07-26 23:07:02 +0900

    디자인 초안(매장 선택, 매장 상세)

---
+++ app/src/main/java/com/example/baeminclone/SelectStoreActivity.kt
@@ -0,0 +1,11 @@
+package com.example.baeminclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_store)
+    }
+}
